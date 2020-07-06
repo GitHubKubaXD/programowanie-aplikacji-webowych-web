79bb2f43ddb77f578372e9b486326e43d34e3df4
GitHubKubaXD <GitHubKubaXD@.github.com>
2020-07-06 12:28:10 +0200

    .

--- starter with webpack/src/index.ts
+++ starter with webpack/src/index.ts
@@ -2,15 +2,45 @@ import './main.scss';
 import { App } from './app';
 import {Field} from "./Field";
 import {FieldLabel} from "./FieldLabel"
-//import {CheckBoxField} from "./CheckBoxField"
+import {CheckBoxField} from "./CheckBoxField"
 import {DateField} from "./DateField"
+import { InputField } from './InputField';
+import { EmailField } from './EmailField';
+import { SelectField } from './SelectField';
+import { TextAreaField } from './TextAreaField';
 
 
-const hello = "yep, it's workin ;laskjf lsdkjf l;kdsjg l;kdsjgklg. still sick";
+/*const hello = "yep, it's workin ;laskjf lsdkjf l;kdsjg l;kdsjgklg. still sick";
 document.body.innerHTML = hello;
 const app = new App();
 
 window.addEventListener('DOMContentLoaded', (event) =>{
-    var XD = new DateField(new FieldLabel("xd"), "ups", "pies", "kotek")
+    var XD = new InputField(new FieldLabel("Imię: "), "input", "input", "")
     document.body.appendChild(XD.render())
 })
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    var XD = new DateField(new FieldLabel("Nazwisko: "), "input", "input", "")
+    document.body.appendChild(XD.render())
+})
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    var XD = new EmailField(new FieldLabel("Email: "), "email", "email", "")
+    document.body.appendChild(XD.render())
+})
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    var XD = new SelectField(new FieldLabel("Wybierz kierunek studiów: "), "select", "select", "")
+    document.body.appendChild(XD.render())
+})
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    var XD = new CheckBoxField(new FieldLabel("Czy preferujesz e-learning?: "), "checkbox", "checkbox", "")
+    document.body.appendChild(XD.render())
+})
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    var XD = new TextAreaField(new FieldLabel("Uwagi: "), "uwagi", "textarea", "")
+    document.body.appendChild(XD.render())
+})
+*/
